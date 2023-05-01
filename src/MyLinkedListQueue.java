f6bc0502383149f89799c64e637e6c8056f5557f
almiinho <almazazamatov863@gmail.com>
2023-05-01 18:59:54 +0600

    ads

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,6 +1,7 @@
 public class MyLinkedListQueue<T> {
     private MyLinkedList<T> list;
     public MyLinkedListQueue(){list = new MyLinkedList<>();}
+    public void enqueue(T element ){list.addLast(element);}
 
 
 }
