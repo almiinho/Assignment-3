90b057ab6b63b7e82d5faf060b35b5c227167cbd
almiinho <almazazamatov863@gmail.com>
2023-05-01 18:50:36 +0600

    ads

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,6 +1,10 @@
 import java.util.EmptyStackException;
 public class MyLinkedListStack<T> {
 private MyLinkedList<T> list;
+public MyLinkedListStack(){
+list = new MyLinkedList<>();
+}
+public void push(T element){list.addFirst(element);}
 
 
 }
