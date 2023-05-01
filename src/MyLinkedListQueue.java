bb884cc5fa9d90c4b14b855a0ca8ddd64bed608c
almiinho <almazazamatov863@gmail.com>
2023-05-01 19:01:37 +0600

    ads

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,7 +1,15 @@
+import java.util.EmptyStackException;
+
 public class MyLinkedListQueue<T> {
     private MyLinkedList<T> list;
     public MyLinkedListQueue(){list = new MyLinkedList<>();}
     public void enqueue(T element ){list.addLast(element);}
+    public T dequeue() throws EmptyStackException{
+        if (list.isEmpty()){
+            throw new EmptyStackException();
+        }
+        return list.removeFirst();
+    }
 
 
 }
