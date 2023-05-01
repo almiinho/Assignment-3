b04f4e75ff7a7616895e4ea2bced386c420aae9f
almiinho <almazazamatov863@gmail.com>
2023-05-01 20:43:29 +0600

    ads

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -4,21 +4,21 @@ private MyLinkedList<T> list;
 public MyLinkedListStack(){
 list = new MyLinkedList<>();
 }
-    public void push(T element){list.addFirst(element);}
-    public T pop()throws EmptyStackException{
+    public void push(T element){list.addFirst(element);} // adds elements to the top of aa stack
+    public T pop()throws EmptyStackException{// returns and removes top element
         if (list.isEmpty()) {
              throw new EmptyStackException();
         }
         return list.removeFirst();
     }
-    public T peek() throws EmptyStackException {
+    public T peek() throws EmptyStackException {//returns top elements
         if (list.isEmpty()) {
             throw new EmptyStackException();
         }
         return list.getFirst();
     }
-    public int size(){return list.size();}
-    public boolean isEmpty(){return list.isEmpty();}
+    public int size(){return list.size();} // returns the size of a stack
+    public boolean isEmpty(){return list.isEmpty();}//checks if stack is empty or not
 
 
 }
