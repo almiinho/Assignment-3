e62566216793c959ac94e963884a6331f8d96fa5
almiinho <almazazamatov863@gmail.com>
2023-05-01 21:07:52 +0600

    ads

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Stack stack = new Stack ();
+        System.out.println(stack.size());
+        stack.push(1);
+        stack.push(4);
+        stack.push(5);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.size());
+        System.out.println(stack.empty());
+        System.out.println(stack.peek());
+        System.out.println(stack.size());
+        System.out.println(stack.pop());
+        System.out.println(stack.size());
+
+
     }
 }
