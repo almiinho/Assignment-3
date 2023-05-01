581251de0eed2f0c2d8cc2c2c96abe6d4aae3bdd
almiinho <almazazamatov863@gmail.com>
2023-05-01 20:41:32 +0600

    ads

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -3,21 +3,21 @@ import java.util.EmptyStackException;
 public class MyLinkedListQueue<T> {
     private MyLinkedList<T> list;
     public MyLinkedListQueue(){list = new MyLinkedList<>();}
-    public void enqueue(T element ){list.addLast(element);}
-    public T dequeue() throws EmptyStackException{
+    public void enqueue(T element ){list.addLast(element);} // adds elements to the back of a queue
+    public T dequeue() throws EmptyStackException{ // returns and removes elements from the front of a queue
         if (list.isEmpty()){
             throw new EmptyStackException();
         }
         return list.removeFirst();
     }
-    public T peek() throws EmptyStackException{
+    public T peek() throws EmptyStackException{ // returns front element of a queue
         if(list.isEmpty()){
             throw new EmptyStackException();
         }
         return list.getFirst();
     }
-    public int size(){return list.size();}
-    public boolean isEmpty(){return list.isEmpty();}
+    public int size(){return list.size();}// returns the size of a queue
+    public boolean isEmpty(){return list.isEmpty();} // checks if queue is empty or not
 
 
 
