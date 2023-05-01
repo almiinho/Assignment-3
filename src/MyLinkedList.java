82a73bd594b647fed8dd47b68c0e2a8f56a0ac9a
almiinho <almazazamatov863@gmail.com>
2023-05-01 18:21:05 +0600

    ads

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -3,6 +3,12 @@ public class MyLinkedList<T> {
     private Node<T> head;
     private int size;
 private static class Node<T>{
+    private T data;
+    private Node<T> next;
 
+    public Node(T data) {
+        this.data = data;
+    }
 }
 }
+
